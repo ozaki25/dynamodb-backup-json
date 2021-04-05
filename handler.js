2a51048704a0eb7543751f12284b92022d78e4bf
ozaki25 <fs65ugumori@yahoo.co.jp>
2021-04-06 01:13:50 +0900

    output json file

--- handler.js
+++ handler.js
@@ -1,3 +1,4 @@
+import { writeFileSync } from 'fs';
 import { DynamoDB } from 'aws-sdk';
 
 const { DYNAMODB_TABLE } = process.env;
@@ -9,22 +10,31 @@ let items = [];
 function scan(lastEvaluatedKey) {
   const params = {
     TableName: DYNAMODB_TABLE,
-    LastEvaluatedKey: lastEvaluatedKey || null,
+    ExclusiveStartKey: lastEvaluatedKey || null,
   };
+  console.log({ params });
   return dynamo.scan(params).promise();
 }
 
 async function getAll(lastEvaluatedKey) {
-  const { Items, LastEvaluatedKey } = await scan(lastEvaluatedKey);
+  const { Items, LastEvaluatedKey, Count } = await scan(lastEvaluatedKey);
+  console.log({ LastEvaluatedKey, Count });
   items = [...items, ...Items];
   if (LastEvaluatedKey) {
-    getAll(LastEvaluatedKey);
+    await getAll(LastEvaluatedKey);
   }
+}
+
+function writeToJSON(filename, data) {
+  console.log({ filename, count: data.length });
+  writeFileSync(filename, JSON.stringify(data));
 }
 
 export async function main() {
   await getAll();
-  console.log(items.length);
-  console.log(items);
+  writeToJSON(
+    `../../${DYNAMODB_TABLE}-${new Date().toISOString()}.json`,
+    items,
+  );
   return { statusCode: 200 };
 }
