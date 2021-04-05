270844e7afbad09a58378c4667ecce732d1490a7
ozaki25 <fs65ugumori@yahoo.co.jp>
2021-04-06 00:52:17 +0900

    get all items

--- handler.js
+++ handler.js
@@ -1,3 +1,30 @@
+import { DynamoDB } from 'aws-sdk';
+
+const { DYNAMODB_TABLE } = process.env;
+
+const dynamo = new DynamoDB.DocumentClient();
+
+let items = [];
+
+function scan(lastEvaluatedKey) {
+  const params = {
+    TableName: DYNAMODB_TABLE,
+    LastEvaluatedKey: lastEvaluatedKey || null,
+  };
+  return dynamo.scan(params).promise();
+}
+
+async function getAll(lastEvaluatedKey) {
+  const { Items, LastEvaluatedKey } = await scan(lastEvaluatedKey);
+  items = [...items, ...Items];
+  if (LastEvaluatedKey) {
+    getAll(LastEvaluatedKey);
+  }
+}
+
 export async function main() {
-  return { statusCode: 200, body: 'Hello' };
+  await getAll();
+  console.log(items.length);
+  console.log(items);
+  return { statusCode: 200 };
 }
